57b197f45c7d36f6b308ee3aeb592b4e4bce4a0e
padrisimo <nopuedoparardemolar@outlook.com>
2017-08-20 16:01:40 +0200

    controlled components and binding context

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,7 @@ import SearchBar from '../containers/search_bar'
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className="m-t-3">
         <SearchBar />
       </div>
     );
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {term: ''};
+
+        this.onInputChange = this. onInputChange.bind(this);
+    }
+    onInputChange(e){
+        console.log(e.target.value);
+        this.setState({ term: e.target.value });
+    }    
     render() {
         return (
             <form className="input-group">
-                <input />
+                <input
+                    placeholder="Get a five-day forecast in your favorite cities"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange} />
                 <span className="input-group-btn">
                     <button className="btn btn-secondary" type="submit">Submit</button>
                 </span>
