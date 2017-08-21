496b491da590c597d97a738ecf6f4408e5a86f60
adrian.fernandez.externo@axa-assistance.es <adrian.fernandez.externo@axa-assistance.es>
2017-08-21 14:43:28 +0200

    building a weatherlist container

---
+++ src/containers/weather_list.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { connect } from 'redux';
+
+class WeatherList extends Component {
+    state = {  }
+    render() {
+        return (
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>City</th>
+                        <th>Temperature</th>
+                        <th>Pressure</th>
+                        <th>Humidity</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        );
+    }
+}
+
+const mapStateToProps = ({ weather }) => {
+     return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
