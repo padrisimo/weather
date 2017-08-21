897122886c99d632118f607f9278c44eeaabd4d9
adrian.fernandez.externo@axa-assistance.es <adrian.fernandez.externo@axa-assistance.es>
2017-08-21 11:42:55 +0200

    ajax requests with axios

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '78d6c912d57a52b05f60809120a8c0cc';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'; 
+
+export function fetchWeather() {
+    const url = `${ROOT_URL}&q=${city},es`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
