adbe4a8cbc7acc634a9d6d3131e83a154246314b
adrian.fernandez.externo@axa-assistance.es <adrian.fernandez.externo@axa-assistance.es>
2017-08-22 15:14:16 +0200

    last shot

--- src/components/app.js
+++ src/components/app.js
@@ -5,9 +5,13 @@ import WeatherList from '../containers/weather_list';
 export default class App extends Component {
   render() {
     return (
-      <div className="m-t-3">
+      <div className="site">
+        <main className="site-content m-t-3">
+        <h1 className="m-b-2">Padrisimo Weather</h1>
         <SearchBar />
         <WeatherList />
+        </main>
+        <footer class="footer"><a href="https://github.com/padrisimo">padrisimo</a></footer>
       </div>
     );
   }
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -27,7 +27,7 @@ class SearchBar extends Component {
         return (
             <form onSubmit={this.onFormSubmit} className="input-group">
                 <input
-                    placeholder="Get a five-day forecast in your favorite cities of Spain"
+                    placeholder="Get a five-day forecast in your favorite cities"
                     className="form-control"
                     value={this.state.term}
                     onChange={this.onInputChange} />
