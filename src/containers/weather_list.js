08ae7ba96b126c90afdd35a4550cedafcc9c8012
adrian.fernandez.externo@axa-assistance.es <adrian.fernandez.externo@axa-assistance.es>
2017-08-22 13:15:49 +0200

    google masp integration

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
     renderWeather(cityData){
@@ -8,10 +9,11 @@ class WeatherList extends Component {
         const temps = cityData.list.map(weather => weather.main.temp);
         const pressure = cityData.list.map(weather => weather.main.pressure);
         const humidities = cityData.list.map(weather => weather.main.humidity);
+        const { lon, lat } = cityData.city.coord;
 
         return (
             <tr key={cityName}>
-                <td>{cityName}</td>
+                <td><GoogleMap lon={lon} lat={lat} /></td>
                 <td>
                   <Chart data={temps} color="black" units="Cº"/>
                 </td>
